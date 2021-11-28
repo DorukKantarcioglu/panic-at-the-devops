a715de0087704f26ce788ba40b981127ee1b714d
Doruk Kantarcıoğlu <dorukkantarcioglu@gmail.com>
2021-11-28 17:36:37 +0300

    Project without Vaadin, will start on the endpoints soon.

---
+++ pandemic-manager/src/main/java/com/panicatthedevops/pandemicmanager/PandemicManagerApplication.java
@@ -0,0 +1,13 @@
+package com.panicatthedevops.pandemicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemicManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandemicManagerApplication.class, args);
+	}
+
+}
---
+++ pandemic-manager/src/test/java/com/panicatthedevops/pandemicmanager/PandemicManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panicatthedevops.pandemicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
