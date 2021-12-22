12b6b946ea69284ed9d5eb862a69ad391f49b8ed
YagmurEryilmaz <yagmur.eryilmaz@ug.bilkent.edu.tr>
2021-12-22 20:46:15 +0300

    menu-tab and some other changes

--- campus-care-frontend/react-app/src/app/Notifications.js
+++ campus-care-frontend/react-app/src/app/Notifications.js
@@ -1,12 +1,21 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 import NotificationList from "./components/NotificationList";
+import StudentList from "./components/StudentList";
 
 function Notifications(props) {
   return (
     <React.Fragment>
-      <div className="row justify-content-center">
-        <NotificationList />
+      <div className="row justify-content-start">
+        <h1 className="display-6 m-2" style={{ fontFamily: "Georgia" }}>
+          Notifications
+        </h1>
+        <div className="col-6 m-0">
+          <NotificationList />
+        </div>
+        <div className="col-6 m-0">
+          <StudentList />
+        </div>
       </div>
     </React.Fragment>
   );
--- campus-care-frontend/react-app/src/app/components/StudentList.js
+++ campus-care-frontend/react-app/src/app/components/StudentList.js
@@ -1,54 +1,43 @@
 import * as ReactDOM from "react-dom";
 import InfoBox from "./InfoBox";
 
+const StudentList = (props) => {
+  const handleShow = (id) => {
+    const path = "/student-info/".concat(id);
+    console.log(path);
+    //history.back()
+    ReactDOM.render(<InfoBox id={id} />, document.getElementById("root"));
+  };
 
-const StudentList=(props)=>
-{
-
-
-    const handleShow =(id)=>
-    {
-        const path = "/student-info/".concat(id)
-        console.log(path)
-        //history.back()
-        ReactDOM.render(
-
-                <InfoBox id = {id} />
-            ,
-            document.getElementById('root')
-        );
-    }
-
-    return(
-
-        <table>
-            <thead>
-            <tr>
-                <th> Name</th>
-                <th> Surname</th>
-                <th> ID Number</th>
-                <th>Risk Status</th>
-            </tr>
-            </thead>
-            <tbody>
-            {props.data &&
-            props.data.map((student) => {
-                return (
-                    <>
-                        <tr>
-                            <td>{student.name}</td>
-                            <td>{student.surname}</td>
-                            <td>{student.id}</td>
-                            <td>{student.allowedOnCampus}</td>
-                            <td><button onClick={()=>handleShow(student.id)} >show</button> </td>
-                        </tr>
-                    </>
-                )
-            })}
-            </tbody>
-        </table>
-
-    )
-
-}
-export default StudentList
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th> Name</th>
+          <th> Surname</th>
+          <th> ID Number</th>
+          <th>Risk Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data &&
+          props.data.map((student) => {
+            return (
+              <>
+                <tr>
+                  <td>{student.name}</td>
+                  <td>{student.surname}</td>
+                  <td>{student.id}</td>
+                  <td>{student.allowedOnCampus}</td>
+                  <td>
+                    <button onClick={() => handleShow(student.id)}>show</button>{" "}
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+      </tbody>
+    </table>
+  );
+};
+export default StudentList;
