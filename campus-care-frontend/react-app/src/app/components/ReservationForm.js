396e6e32f24477801ae6730ba84c0af233bd00da
Suleyman Hanyyev <suleyman.hanyyev@ug.bilkent.edu.tr>
2021-12-23 04:11:24 +0300

    Created form for makeReservations()

---
+++ campus-care-frontend/react-app/src/app/components/ReservationForm.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import {Container, Col, Row} from 'react-bootstrap'
+
+function ReservationForm() {
+    return (
+        <Container>
+            <Row>
+                <Col sm ={4}>
+                    <h5> Appointment System</h5>
+                    <Button>
+                        Sports Center
+                    </Button>
+                    <br/>
+                    <Button>
+                        Library
+                    </Button>
+                </Col>
+                <Col sm = {8}>
+                    <Form>
+
+                        <Form.Group as={Col} >
+                            <Form.Label>Sports Hall</Form.Label>
+                            <Form.Select defaultValue="Choose..." >
+                                <option>Main Sports Hall</option>
+                                <option>Dormitory Sports Hall</option>
+                            </Form.Select>
+                        </Form.Group>
+
+
+                        <Form.Group className="mb-3" controlId="formGridAddress2">
+                            <Form.Label>Address 2</Form.Label>
+                            <Form.Control placeholder="Apartment, studio, or floor" />
+                        </Form.Group>
+
+                        <Form.Group>
+                                <Form.Label>Time Interval</Form.Label>
+                                <Form.Select defaultValue="Choose...">
+                                    <option>10:30</option>
+                                    <option>...</option>
+                                </Form.Select>
+                        </Form.Group>
+
+                        <Row className="mb-2">
+                            <Col>
+                                <Button variant="primary" type="submit">
+                                    Reserve
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="submit">
+                                    Cancel
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default ReservationForm;
--- campus-care-frontend/react-app/src/app/components/SideBar.js
+++ campus-care-frontend/react-app/src/app/components/SideBar.js
@@ -14,22 +14,6 @@ const SideBar = (props) => {
           </Link>
         </li>
         <li className="side">
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Dropdown Button
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Link to="/profile-info" className="side">
-            Profile Info
-          </Link>
-        </li>
-        <li className="side">
           <Link to="/courses" className="side">
             Courses
           </Link>
